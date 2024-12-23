595695f19f8495bf7299f4160807d90cb4c0dc6f
daeekim <asdxl1030@naver.com>
2024-12-23 19:25:41 +0900

    Initial Commit

---
+++ TimeCheck/TimeCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCheckApp.swift
+//  TimeCheck
+//
+//  Created by 김대언 on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeCheckTests/TimeCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCheckTests.swift
+//  TimeCheckTests
+//
+//  Created by 김대언 on 12/23/24.
+//
+
+import Testing
+@testable import TimeCheck
+
+struct TimeCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
